9fe421e53d67abe3cf4494a996c53ff4bd4cadbe
Maik Römpagel <maik.roempagel@gmail.com>
2021-04-22 09:09:31 +0200

    Deactivate md preview on buffer enter

--- nvim/.config/nvim/lua/markdown-preview.lua
+++ nvim/.config/nvim/lua/markdown-preview.lua
@@ -2,7 +2,7 @@ local g = vim.g
 
 -- set to 1, nvim will open the preview window after entering the markdown buffer
 -- default: 0
-g.mkdp_auto_start = 1
+g.mkdp_auto_start = 0
 
 -- set to 1, the nvim will auto close current preview window when change
 -- from markdown buffer to another buffer
