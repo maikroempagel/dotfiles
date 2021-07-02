b0714ec1b634fd381efc13368ca571b2bee72ff1
Maik Römpagel <maik.roempagel@gmail.com>
2021-07-02 21:27:38 +0200

    Add telescope keymap to find files in nvim config

--- nvim/.config/nvim/lua/telescope-settings.lua
+++ nvim/.config/nvim/lua/telescope-settings.lua
@@ -16,6 +16,7 @@ t.load_extension('fzy_native')
 local mk = require('helpers').mapkey
 
 mk('n', '<leader>ff', "<cmd>lua require('telescope.builtin').find_files()<CR>")
+mk('n', '<leader>fn', "<cmd>lua require('telescope.builtin').find_files({cwd = '~/.config/nvim'})<CR>")
 mk('n', '<leader>fg', "<cmd>lua require('telescope.builtin').live_grep()<CR>")
 mk('n', '<leader>fb', "<cmd>lua require('telescope.builtin').buffers()<CR>")
 mk('n', '<leader>fh', "<cmd>lua require('telescope.builtin').help_tags()<CR>")
